521454266f79dd430bc0c3226e5695ede7c79158
kamath7 <adithyakamath96@gmail.com>
2022-06-10 03:15:16 +0530

    cosmetic changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -52,8 +52,7 @@ const Contact = () => {
             Contact Me
           </h2>
           <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
+            If you have a great internship/full time opportunity and I suit the profile, kindly contact me using the below form!
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
