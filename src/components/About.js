8e784be166b50119ea1411de6d7ef61c77ef6d6b
kamath7 <adithyakamath96@gmail.com>
2022-06-10 02:14:13 +0530

    navbar added

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ const About = () => {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hello! I'm Jane Doe
-            <br className="hidden lg:inline-block" /> I am a Senior Software Engineer!
+            Hello! I'm Jane Doe,
+            <br className="hidden lg:inline-block" /> a Senior Software Engineer!
           </h1>
           <p className='mb-8 leading-relaxed"'>
          I get geeked out about tech because I love feeling smart after solving a problem. Well, who doesn't.
