83d15467f742b214c5c0209b4adc14cae17efdb7
kamath7 <adithyakamath96@gmail.com>
2022-06-10 02:04:14 +0530

    projects added

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const projects = [
+  {
+    title: "DBMS Scripting",
+    subtitle: "All the DDMS scripts",
+    description: "All the scripts that I wrote to query our store database",
+    image: "https://bs-uploads.toptal.io/blackfish-uploads/blog/post/seo/og_image_file/og_image/15493/0712-Bad_Practices_in_Database_Design_-_Are_You_Making_These_Mistakes_Dan_Social-754bc73011e057dc76e55a44a954e0c3.png",
+    link: "https://github.com/rithukrishnan/dbms/blob/main/SQL",
+  },
+  {
+    title: "Netconf Python API",
+    subtitle: "NETCONF python library ncclient",
+    description: "Python APIs to communicate with a linux box via NETCONF python library ncclient.",
+    image: "https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/686219/retina_1708x683_cover-0404-python-class-attributes-an-overly-thorough-guide-Waldek_Newsletter-cc8ad72af748c8f2817b4855db358c43.png",
+    link: "https://github.com/rithukrishnan/py-api",
+  }
+];
